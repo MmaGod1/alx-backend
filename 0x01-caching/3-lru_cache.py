f37808aef74aa373c41de9d9cbdba5c0130c3665
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-11-13 18:56:39 +0100

    Create 3-lru_cache.py

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" LRU cache replacement system
+"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ LRU cache module."""
+    def __init__(self):
+        """Initialize LRUCache."""
+        super().__init__()
+        self.order = []  # To keep track of insertion order
+
+    def put(self, key, item):
+        """Add item to cache with LRU replacement if necessary."""
+        if key is not None and item is not None:
+            # If key already exists, remove it from the order to update its position
+            if key in self.cache_data:
+                self.order.remove(key)
+
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                lru_key = self.order.pop(0)  # Remove the first item in order
+                del self.cache_data[lru_key]
+                print(f"DISCARD: {lru_key}")
+
+            self.cache_data[key] = item
+            self.order.append(key)
+
+    def get(self, key):
+        """Retrieve item from cache by key."""
+        if key is not None:
+            return self.cache_data.get(key)
+        return None
