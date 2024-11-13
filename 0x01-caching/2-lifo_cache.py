fa7fe4c859c0f9c9ae2843afbf406ec3aa146ea1
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-11-13 16:29:24 +0100

    Create 2-lifo_cache.py

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" LIFO Cache replacement system
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ LIFO cache module."""
+    def __init__(self):
+        """Initialize LIFOCache."""
+        super().__init__()
+        self.order = []  # To keep track of insertion order
+
+    def put(self, key, item):
+        """Add item to cache with LIFO replacement if necessary."""
+        if key is not None and item is not None:
+            if key not in self.cache_data:
+                self.order.append(key)
+            self.cache_data[key] = item
+
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                recent_key = self.order.pop()
+                del self.cache_data[recent_key]
+                print(f"DISCARD: {recent_key}")
+
+    def get(self, key):
+        """Retrieve item from cache by key."""
+        if key is not None:
+            return self.cache_data.get(key)
+        return None
