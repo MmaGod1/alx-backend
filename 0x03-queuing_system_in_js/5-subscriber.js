7e60106541b59d72b57479436ab1ccec7f9ee129
MmaGod1 <gmmaduchukwu@gmail.com>
2024-12-05 10:58:19 +0000

    Publish and subscribe to channel

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,23 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+const channel = 'ALXchannel';
+client.subscribe(channel);
+
+client.on('message', (channel, message) => {
+  console.log(`Received message: ${message}`);
+  
+  if (message === 'KILL_SERVER') {
+    client.unsubscribe(channel);
+    client.quit();
+  }
+});
