8806f56d6688971ca62fcb9d94067570a023039a
MmaGod1 <gmmaduchukwu@gmail.com>
2024-12-05 00:33:11 +0000

    redis update

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -13,14 +13,18 @@ const client = createClient();
   await client.connect();
 })();
 
-client.hSet('HolbertonSchools', 'Portland', '50', print);
-client.hSet('HolbertonSchools', 'Seattle', '80', print);
-client.hSet('HolbertonSchools', 'New York', '20', print);
-client.hSet('HolbertonSchools', 'Bogota', '20', print);
-client.hSet('HolbertonSchools', 'Cali', '40', print);
-client.hSet('HolbertonSchools', 'Paris', '2', print);
+client.hset(
+  'HolbertonSchools',
+  'Portland', '50',
+  'Seattle', '80',
+  'New York', '20',
+  'Bogota', '20',
+  'Cali', '40',
+  'Paris', '2',
+  print
+);
 
-client.hGetAll('HolbertonSchools', function (error, result) {
+client.hgetall('HolbertonSchools', function (error, result) {
   if (error) {
     console.log(error);
     throw error;
